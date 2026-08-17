aff3cc23d6579c837636d8d89aa3d43602572e2e
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2026-08-17 18:40:07 +0530

    Update sender.cpp

--- Linux/Kernel/Code/IPC/Message_queue/sender.cpp
+++ Linux/Kernel/Code/IPC/Message_queue/sender.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <mqueue.h>
 #include <cstring>
+#include <fcntl.h>
 
 int main() {
     const char* name = "/myqueue";
 
-    mqd_t mq = mq_open(name, O_CREAT | O_WRONLY, 0666, nullptr);
+    struct mq_attr attr{};
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 100;
+
+    mqd_t mq = mq_open(name, O_CREAT | O_WRONLY, 0666, &attr);
+
+    if (mq == (mqd_t)-1) {
+        perror("mq_open");
+        return 1;
+    }
 
     const char* msg = "Hello from sender!";
-    mq_send(mq, msg, strlen(msg) + 1, 0);
+
+    if (mq_send(mq, msg, strlen(msg) + 1, 0) == -1) {
+        perror("mq_send");
+        return 1;
+    }
 
     std::cout << "Message sent\n";
 
     mq_close(mq);
+
     return 0;
 }
